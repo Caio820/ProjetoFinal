c771394f0155a4f3b1fd71d788b84f9149994a1e
Caio820 <caio.lds123@gmail.com>
2019-09-28 16:58:16 -0300

    Alterações de tela.

--- app/src/main/java/com/example/projetofinal/TelaPrincipalActivity.java
+++ app/src/main/java/com/example/projetofinal/TelaPrincipalActivity.java
@@ -2,13 +2,27 @@ package com.example.projetofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TelaPrincipalActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabPerfil, fabAdicionar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+        fabPerfil = findViewById(R.id.floatingActionButtonPerfil);
+        fabPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TelaPrincipalActivity.this, PerfilActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
